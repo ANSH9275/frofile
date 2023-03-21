d9599cb0d96e17f91a18def0a209521870497e17
ANSH9275 <120919631+ANSH9275@users.noreply.github.com>
2023-03-22 02:07:49 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var tayped = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "BCA Student", "web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
